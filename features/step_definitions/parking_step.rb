7889b7ba7c29a24d5012b3ff7158b54941bbed4c
OFFz19 <aob.zenon@gmail.com>
2018-08-19 14:58:08 +0700

    define condition and testing result hour for payment

--- features/step_definitions/parking_step.rb
+++ features/step_definitions/parking_step.rb
@@ -2,7 +2,8 @@ require 'time'
 
 class Cost
     def diff(startTime, endTime)
-        diffTime = Time.parse(endTime) - Time.parse(startTime)
+        diffTime = (Time.parse(endTime) - Time.parse(startTime)) / 3600
+        # puts startTime, endTime, diffTime
         if diffTime <= 1
             return 25
         end
@@ -10,22 +11,32 @@ class Cost
             return 50
         end
         if diffTime > 2 && diffTime <= 3
-            return 50
+            return 80
         end
         if diffTime > 3 && diffTime <= 4
-            return 50
+            return 110
         end
-        
+        if diffTime > 4 && diffTime <= 5
+            return 145
+        end
+        if diffTime > 5 && diffTime <= 6
+            return 180
+        end
+        if diffTime > 6 && diffTime <= 24
+            return 250
+        end
     end
 end
 
 Given("I got parking ticket at time {string}") do |startTime|
-    @startTime = Time.at(startTime.to_i).utc.strftime("%H:%M:%S")
+    # @startTime = Time.at(startTime).utc.strftime("%H:%M:%S")
+    @startTime = startTime
     @cost = Cost.new
 end
   
   When("I return parking at {string}") do |endTime|
-    @endTime = Time.at(endTime.to_i).utc.strftime("%H:%M:%S")
+    # @endTime = Time.at(endTime).utc.strftime("%H:%M:%S")
+    @endTime = endTime
     @actual = @cost.diff @startTime, @endTime
 end
   
